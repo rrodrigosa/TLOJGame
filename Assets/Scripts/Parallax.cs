4fb5a9c28ca74ab488d6d00fb07182286e6fad45
RodrigoSA <rrodrigo.sa@gmail.com>
2020-06-18 12:50:36 -0300

    Update parallax script to add option for the vertical effect

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float length, startpos;
+    private float length, horizontalStartPos, verticalStartPos;
     public GameObject cam;
     [Tooltip("0 = no effect \n1 = full effect (follows the camera)")]
     [Range(0, 1)]
-    public float parallaxEffect;
+    public float horizontalParallaxEffect;
+    [Tooltip("0 = no effect \n1 = full effect (follows the camera)")]
+    [Range(0, 1)]
+    public float verticalParallaxEffect;
     [Tooltip("Needs SpriteRenderer component")]
     public bool repeatBg = false;
 
     void Start()
     {
-        startpos = transform.position.x;
+        horizontalStartPos = transform.position.x;
+        verticalStartPos = transform.position.y;
         if (repeatBg)
         {
             length = GetComponent<SpriteRenderer>().bounds.size.x;
@@ -22,24 +26,35 @@ public class Parallax : MonoBehaviour
     }
     void Update()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        HorizontalEffect();
+        VerticalEffect();
+    }
 
+    void HorizontalEffect()
+    {
+        float temp = (cam.transform.position.x * (1 - horizontalParallaxEffect));
+        float dist = (cam.transform.position.x * horizontalParallaxEffect);
+        transform.position = new Vector3(horizontalStartPos + dist, transform.position.y, transform.position.z);
         RepeatBackground(repeatBg, temp);
+    }
+
+    void VerticalEffect()
+    {
+        float verticalDist = (cam.transform.position.y * verticalParallaxEffect);
+        transform.position = new Vector3(transform.position.x, verticalStartPos + verticalDist, transform.position.z);
     }
 
     void RepeatBackground(bool value, float temp)
     {
         if (value)
         {
-            if (temp > startpos + length)
+            if (temp > horizontalStartPos + length)
             {
-                startpos += length;
+                horizontalStartPos += length;
             }
-            else if (temp < startpos - length)
+            else if (temp < horizontalStartPos - length)
             {
-                startpos -= length;
+                horizontalStartPos -= length;
             }
         }
     }
